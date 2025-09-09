7f290b0a8c0f0cd888c2222936158c14f1809214
KiryanovKD <kiryanov1997@gmail.com>
2025-09-09 18:38:40 +0400

    fix: update runtime version

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -190,7 +190,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: alloc::borrow::Cow::Borrowed("mubert-runtime"),
     impl_name: alloc::borrow::Cow::Borrowed("mubert-runtime"),
     authoring_version: 1,
-    spec_version: 6,
+    spec_version: 7,
     impl_version: 0,
     apis: apis::RUNTIME_API_VERSIONS,
     transaction_version: 1,
